62b50d87ac68a50b39155c27357c6709e764ec5a
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2012-09-19 09:42:55 +0000

    - Fix for creating trackerless swift URLs (Reported by Raynor).
    git-svn-id: http://svn.tribler.org/libswift/branches/arno/swift-like-ftp@28267 001aeff7-3401-0410-a489-f7902fc005dd

--- swift.cpp
+++ swift.cpp
@@ -432,11 +432,21 @@ int HandleSwiftFile(std::string filename, Sha1Hash root_hash, std::string tracke
 
 		if (swift::Complete(single_fd) == 0)
 			quit("cannot open empty file %s",filename.c_str());
+	  
+                std::ostringstream oss;
+	        oss << "tswift:";
+		if (trackerargstr != "")
+	    	    oss << "//" << trackerargstr;
+	        oss << "/" << RootMerkleHash(single_fd).hex();
+ 		if (chunk_size != SWIFT_DEFAULT_CHUNK_SIZE)
+	  	    oss << "$" << chunk_size;
+	        oss << "\n";
+       	        
+	        std::stringbuf *pbuf=oss.rdbuf();
+	        if (pbuf == NULL)
+	             print_error("cannot create URL");
 		int ret = 0;
-		if (chunk_size == SWIFT_DEFAULT_CHUNK_SIZE)
-			ret = fprintf(fp,"tswift://%s/%s\n", trackerargstr.c_str(), RootMerkleHash(single_fd).hex().c_str());
-		else
-			ret = fprintf(fp,"tswift://%s/%s$%i\n", trackerargstr.c_str(), RootMerkleHash(single_fd).hex().c_str(), chunk_size);
+		ret = fprintf(fp,"%s", pbuf->str().c_str());
 		if (ret <0)
 			print_error("cannot write URL");
 
