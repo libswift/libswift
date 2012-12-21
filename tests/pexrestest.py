fca2a4fedefec0d3dd57fbc40283bb40b689e760
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2012-12-21 15:35:37 +0000

    - Temp disable of IPv6.
    git-svn-id: http://svn.tribler.org/libswift/branches/ppsp-03@30488 001aeff7-3401-0410-a489-f7902fc005dd

--- tests/pexrestest.py
+++ tests/pexrestest.py
@@ -123,7 +123,8 @@ class TestPexRes4cert(TstPexResFramework):
 
 
 
-
+"""
+ARNOTODO: When jenkins gets IPV6, reenable
 class TestPexRes6(TstPexResFramework):
 
     def setUpPreSession(self):
@@ -137,12 +138,12 @@ class TestPexRes6(TstPexResFramework):
         # Fake peer to send as PEX_RES
         myaddr2 = ("::1",5354)
         self.do_tst_reply(socket.AF_INET6,myaddr,myaddr2)
-
+"""
     
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestPexRes4cert))
-    suite.addTest(unittest.makeSuite(TestPexRes6))
+    #suite.addTest(unittest.makeSuite(TestPexRes6))
     return suite
 
 
