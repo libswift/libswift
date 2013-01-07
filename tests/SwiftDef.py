5a86f7bb5a420d6907c6808c90349e674c22e3a2
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2013-01-07 12:31:12 +0000

    - Multifile correction.
    git-svn-id: http://svn.tribler.org/libswift/branches/ppsp-03@30584 001aeff7-3401-0410-a489-f7902fc005dd

--- tests/SwiftDef.py
+++ tests/SwiftDef.py
@@ -383,3 +383,29 @@ def parse_url(url):
         duration = None
 
     return (roothash,tracker,chunksize,duration)
+
+
+# From Core/Swift/util.py
+MULTIFILE_PATHNAME = "META-INF-multifilespec.txt"
+    
+def filelist2swiftspec(filelist):    
+    # TODO: verify that this gives same sort as C++ CreateMultiSpec
+    filelist.sort()    
+        
+    specbody = ""
+    totalsize = 0L
+    for pathname,flen in filelist:
+        specbody += pathname+" "+str(flen)+"\n"
+        totalsize += flen
+        
+    specsize = len(MULTIFILE_PATHNAME)+1+0+1+len(specbody)
+    numstr = str(specsize)
+    numstr2 = str(specsize+len(str(numstr)))
+    if (len(numstr) == len(numstr2)):
+        specsize += len(numstr)
+    else:
+        specsize += len(numstr)+(len(numstr2)-len(numstr))
+    
+    spec = MULTIFILE_PATHNAME+" "+str(specsize)+"\n"
+    spec += specbody
+    return spec
