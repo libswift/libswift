bda7a62623885e8f51dbd5281453d62aaa95e127
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2013-01-14 12:18:57 +0000

    - Added tests covering:   * single file from multi-file download via CMDGW   * CHECKPOINT of swarm via CMDGW.   * MOREINFO with actual connection via CMDGW.
    git-svn-id: http://svn.tribler.org/libswift/branches/ppsp-03@30796 001aeff7-3401-0410-a489-f7902fc005dd

--- tests/vodmftest.py
+++ tests/vodmftest.py
@@ -14,6 +14,7 @@ import subprocess
 import urllib2
 import string
 import binascii
+import json
 from sha import sha
 from traceback import print_exc
 
@@ -69,7 +70,6 @@ class TestVODMultiFile(TestAsNPeers):
             
             print >>sys.stderr,"test: Creating",fullpath,fn
             sdef.add_content(fullpath,fn)
-            self.vodinputsize += s
 
         self.specfn = sdef.finalize(self.peers[0].binpath,destdir=self.peers[0].zerosdir)
         f = open(self.specfn,"rb")
@@ -127,23 +127,28 @@ class TestVODMultiFile(TestAsNPeers):
                 pass
 
 
-    def test_vod_multifile(self):
+    def test_vod_multifile_all(self):
         # Start peer1 as VOD downloader
         CMD = "START tswift://127.0.0.1:"+str(self.peers[0].listenport)+"/"+binascii.hexlify(self.swarmid)+" "+self.peers[1].destdir+"\r\n"
         
-        print >>sys.stderr,"SENDING",CMD
+        print >>sys.stderr,"test: SENDING",CMD
         
         self.peers[1].cmdsock.send(CMD)
         
         # Let peers interact
         self.gotcallback = False
-        self.gotall = False
+        self.gotdllist = False
         self.count = 0
-        self.process_cmdsock(self.peers[1].cmdsock,self.vod_readline)
-        self.assertTrue(self.gotcallback)
-        self.assertTrue(self.gotall)
+        dlfilelist = self.filelist
+        for fn,s in dlfilelist:
+            self.vodinputsize += s
+        vod_readline_lambda = lambda caller,cmd:self.vod_readline(caller,cmd,dlfilelist)
         
-    def vod_readline(self,caller,cmd):
+        self.process_cmdsock(self.peers[1].cmdsock,vod_readline_lambda)
+        self.assertTrue(self.gotcallback)
+        self.assertTrue(self.gotdllist)
+        
+    def vod_readline(self,caller,cmd,dlfilelist):
         self.gotcallback = True
         print >>sys.stderr,"test: vod_readline: Got",`cmd`
         if cmd.startswith("INFO"):
@@ -176,7 +181,7 @@ class TestVODMultiFile(TestAsNPeers):
                 if seqcomp > 0 and seqcomp == dynasize and dynasize > self.vodinputsize:
                     
                     # Compare original and downloaded content
-                    for fn,s in self.filelist:
+                    for fn,s in dlfilelist:
                         osfn = fn.replace("/",os.sep)
                         srcfullpath = os.path.join(self.peers[0].zerosdir,osfn)
                         dstfullpath = os.path.join(self.peers[1].destdir,osfn)
@@ -192,7 +197,7 @@ class TestVODMultiFile(TestAsNPeers):
                         f.close()
                         g.close()
                         
-                    self.gotall = True
+                    self.gotdllist = True
                     self.stop = True
                 
             except:
@@ -201,6 +206,103 @@ class TestVODMultiFile(TestAsNPeers):
             
                 
         return 0
+        
+
+    def test_vod_multifile_file0(self):
+        return self._test_vod_multifile_file(0)
+        
+    def test_vod_multifile_file1(self):
+        return self._test_vod_multifile_file(1)
+
+    def test_vod_multifile_file2(self):
+        return self._test_vod_multifile_file(2)
+        
+    def _test_vod_multifile_file(self,fidx):
+        # Start peer1 as VOD downloader
+        CMD = "START tswift://127.0.0.1:"+str(self.peers[0].listenport)+"/"+binascii.hexlify(self.swarmid)+"/"+self.filelist[fidx][0]+" "+self.peers[1].destdir+"\r\n"
+        
+        print >>sys.stderr,"test: SENDING",CMD
+        self.peers[1].cmdsock.send(CMD)
+        
+        # Let peers interact
+        self.gotcallback = False
+        self.gotdllist = False
+        self.count = 0
+        dlfilelist = [self.filelist[fidx]]
+        for fn,s in dlfilelist:
+            self.vodinputsize += s
+
+        vod_readline_lambda = lambda caller,cmd:self.vod_readline(caller,cmd,dlfilelist)
+        self.process_cmdsock(self.peers[1].cmdsock,vod_readline_lambda)
+        self.assertTrue(self.gotcallback)
+        self.assertTrue(self.gotdllist)
+
+        CMD = "CHECKPOINT "+binascii.hexlify(self.swarmid)+"\r\n"
+        print >>sys.stderr,"test: SENDING",CMD
+        self.peers[1].cmdsock.send(CMD)
+
+        time.sleep(2)
+        
+        mbinfn = os.path.join(self.peers[1].destdir,"clientdump.mbinmap")
+        f = open(mbinfn,"rb")
+        for line in f.readlines():
+            words = line.split()
+            if words[0] == "version":
+                self.assertEquals(words[1],"1")
+            if words[0] == "root" and words[1] == "hash":
+                self.assertEquals(words[2],binascii.hexlify(self.swarmid))
+            if words[0] == "chunk":
+                self.assertEquals(words[2],"1024")
+            if words[0] == "complete":
+                self.assertTrue(int(words[1]) >= self.vodinputsize) # swift will keep on downloading after
+            if words[0] == "completec":
+                self.assertTrue(int(words[1]) >= (self.vodinputsize/1024))
+        f.close()
+
+
+    def test_vod_multifile_all_moreinfo(self):
+        # Start peer1 as VOD downloader
+        CMD = "START tswift://127.0.0.1:"+str(self.peers[0].listenport)+"/"+binascii.hexlify(self.swarmid)+" "+self.peers[1].destdir+"\r\n"
+        CMD += "SETMOREINFO "+binascii.hexlify(self.swarmid)+" 1\r\n"
+        
+        self.peers[1].cmdsock.send(CMD)
+        
+        self.gotinfo = False
+        self.gotplay = False
+        self.gotmoreinfo = False
+        self.process_cmdsock(self.peers[1].cmdsock,self.setmoreinfo_readline)
+        self.assertTrue(self.gotinfo and self.gotplay and self.gotmoreinfo)
+        
+    def setmoreinfo_readline(self,caller,cmd):
+        print >>sys.stderr,"test: setmoreinfo_readline: Got",`cmd`
+        if cmd.startswith("INFO"):
+            self.gotinfo = True
+        if cmd.startswith("PLAY"):
+            self.gotplay = True
+        if cmd.startswith("MOREINFO"):
+            try:
+                words = cmd.split()
+            
+                hashhex = words[1]
+                self.assertEquals(hashhex,binascii.hexlify(self.swarmid))
+                
+                jsondata = cmd[len("MOREINFO ")+40+1:]
+                midict = json.loads(jsondata)
+
+                self.assertTrue(len(midict['channels']) > 0)
+                chan0 = midict['channels'][0]
+                self.assertEquals(chan0['ip'],"127.0.0.1")
+                self.assertEquals(chan0['port'],self.peers[0].listenport)
+
+                self.gotmoreinfo = True
+            except:
+                print_exc()
+                self.assertEquals("MOREINFO","does not contain correct JSON")
+            
+        if self.gotinfo and self.gotplay and self.gotmoreinfo:            
+            self.stop = True
+        return 0
+
         
 
     # From FastI2I.py
