7443af69a5e9f1a655ad1807f2f373ba677fb551
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2013-05-22 11:12:36 +0000

    - Fix for Android build.
    git-svn-id: http://svn.tribler.org/libswift/branches/ppsp-03@32420 001aeff7-3401-0410-a489-f7902fc005dd

--- svn-revision.h
+++ svn-revision.h
@@ -1,1 +1,1 @@
-std::string SubversionRevisionString = "https://svn.tribler.org/libswift/branches/mainbranch@28974";
+std::string SubversionRevisionString = "https://svn.tribler.org/libswift/branches/ppsp-03@32419";
