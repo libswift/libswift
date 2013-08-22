d40ffbb0175da3321a30a481b773aac8c469efc5
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2013-08-22 13:48:11 +0000

    - Linux works with CIPM NONE and no OpenSSL.
    git-svn-id: http://svn.tribler.org/libswift/branches/arno/swift-umt-nchunksig@32746 001aeff7-3401-0410-a489-f7902fc005dd

--- zerostate.cpp
+++ zerostate.cpp
@@ -169,7 +169,8 @@ int ZeroState::Find(const Sha1Hash &root_hash)
         return -1;
 
     // Open as ZeroState
-    return swift::Open(file_name, SwarmID(root_hash), Address(), false, POPT_CONT_INT_PROT_MERKLE, true);
+    SwarmID swarmid(root_hash);
+    return swift::Open(file_name, swarmid, Address(), false, POPT_CONT_INT_PROT_MERKLE, true);
 }
 
 
