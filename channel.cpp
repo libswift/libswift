aa20c35f749bb6ce7d36d07018df9ea6c0826cf7
Riccardo Petrocco <r.petrocco@gmail.com>
2014-02-13 19:57:38 +0100

    consider losses on small queues

--- channel.cpp
+++ channel.cpp
@@ -49,7 +49,7 @@ Channel::Channel(ContentTransfer* transfer, int socket, Address peer_addr) :
     peer_(peer_addr), socket_(socket==INVALID_SOCKET?default_socket():socket), // FIXME
     transfer_(transfer), own_id_mentioned_(false),
     ack_in_right_basebin_(bin_t::NONE),
-    data_in_(TINT_NEVER,bin_t::NONE), data_in_dbl_(bin_t::NONE),
+    data_in_(TINT_NEVER,bin_t::NONE), data_in_dbl_(bin_t::NONE), data_out_size_(0),
     data_out_cap_(bin_t::ALL),hint_in_size_(0), hint_out_size_(0), hint_queue_out_size_(0),
     // Gertjan fix 996e21e8abfc7d88db3f3f8158f2a2c4fc8a8d3f
     // "Changed PEX rate limiting to per channel limiting"
