c3183e95b0a142a20e8baf7510699e9782803cd1
Riccardo Petrocco <r.petrocco@gmail.com>
2014-03-08 11:35:26 +0100

    debug

--- storage.cpp
+++ storage.cpp
@@ -22,6 +22,9 @@ using namespace swift;
 const std::string Storage::MULTIFILE_PATHNAME = "META-INF-multifilespec.txt";
 const std::string Storage::MULTIFILE_PATHNAME_FILE_SEP = "/";
 
+#define DEBUGSTORAGE     0
+
+
 Storage::Storage(std::string ospathname, std::string destdir, int td, uint64_t live_disc_wnd_bytes) :
 	Operational(),
 	state_(STOR_STATE_INIT),
@@ -111,8 +114,8 @@ Storage::~Storage()
 
 ssize_t  Storage::Write(const void *buf, size_t nbyte, int64_t offset)
 {
-    dprintf("%s %s storage: Write: fd %d nbyte " PRISIZET " off %" PRIi64 " state %" PRIi32 "\n", tintstr(), roothashhex().c_str(), single_fd_, nbyte,offset,state_);
-    //fprintf(stderr,"%s %s storage: Write: fd %d nbyte %d off %" PRIi64 " state %d\n", tintstr(), roothashhex().c_str(), single_fd_, nbyte,offset,state_);
+    if (DEBUGSTORAGE)
+        dprintf("%s %s storage: Write: fd %d nbyte " PRISIZET " off %" PRIi64 " state %" PRIi32 "\n", tintstr(), roothashhex().c_str(), single_fd_, nbyte,offset,state_);
 
     if (state_ == STOR_STATE_SINGLE_FILE)
     {
@@ -122,13 +125,15 @@ ssize_t  Storage::Write(const void *buf, size_t nbyte, int64_t offset)
     {
 	int64_t newoff = offset % live_disc_wnd_bytes_;
 
-	dprintf("%s %d ?data writing disk %" PRIi64 " window %" PRIu64 "\n",tintstr(), 0, newoff, live_disc_wnd_bytes_ );
+	if (DEBUGSTORAGE)
+	    dprintf("%s %d ?data writing disk %" PRIi64 " window %" PRIu64 "\n",tintstr(), 0, newoff, live_disc_wnd_bytes_ );
 
 	if (newoff+nbyte > live_disc_wnd_bytes_)
 	{
 	    // Writing more than window
 	    size_t firstbyte = live_disc_wnd_bytes_ - newoff;
-	    dprintf("%s %d ?data writing disk %" PRIi64 " firstbyte " PRISIZET "\n",tintstr(), 0, newoff, firstbyte );
+	    if (DEBUGSTORAGE)
+	        dprintf("%s %d ?data writing disk %" PRIi64 " firstbyte " PRISIZET "\n",tintstr(), 0, newoff, firstbyte );
 	    int ret = pwrite(single_fd_, buf, firstbyte, newoff);
 	    if (ret < 0)
 		return ret;
@@ -149,7 +154,8 @@ ssize_t  Storage::Write(const void *buf, size_t nbyte, int64_t offset)
             //return -1;
         }
 
-        dprintf("%s %s storage: Write: chunk 0\n", tintstr(), roothashhex().c_str() );
+        if (DEBUGSTORAGE)
+            dprintf("%s %s storage: Write: chunk 0\n", tintstr(), roothashhex().c_str() );
 
         // Check for multifile spec. If present, multifile, otherwise single
         if (!strncmp((const char *)buf,MULTIFILE_PATHNAME.c_str(),strlen(MULTIFILE_PATHNAME.c_str())))
