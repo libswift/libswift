18446d6cc697997ec444f65a495cd455531f97f7
Arno Bakker <arno@cs.vu.nl>
2014-04-02 16:24:36 +0200

    - Fix.

--- firefox/demo/seeder-download.bat
+++ firefox/demo/seeder-download.bat
@@ -1,2 +1,1 @@
-D:\Build\github\libswift\libswift\firefox\demo>"c:\Program Files\GnuWin32\bin\wg
-et.exe" http://download.blender.org/durian/trailer/sintel_trailer-720p.ogv
+"c:\Program Files\GnuWin32\bin\wget.exe" http://download.blender.org/durian/trailer/sintel_trailer-720p.ogv
